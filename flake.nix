31c3b0a39237889da03d17bc22adcc6ea19dc0b2
Farid Zakaria <farid.m.zakaria@gmail.com>
2025-10-21 08:46:35 -0700

    move to meson

--- flake.nix
+++ flake.nix
@@ -6,43 +6,71 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { 
-          inherit system;
-          overlays = [
-            (final: prev: {
-              kaitai-struct-cpp-stl-runtime = prev.stdenv.mkDerivation rec {
-                pname = "kaitai-struct-cpp-stl-runtime";
-                version = "0.11";
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (final: prev: {
+            kaitai-struct-cpp-stl-runtime = prev.stdenv.mkDerivation (finalAttrs: {
+              pname = "kaitai-struct-cpp-stl-runtime";
+              version = "0.11";
 
-                src = prev.fetchFromGitHub {
-                  owner = "kaitai-io";
-                  repo = "kaitai_struct_cpp_stl_runtime";
-                  rev = "${version}";
-                  sha256 = "sha256-2glGPf08bkzvnkLpQIaG2qiy/yO+bZ14hjIaCKou2vU=";
-                };
-
-                buildInputs = [ prev.cmake prev.gtest prev.zlib.dev];
-
-                meta = with prev.lib; {
-                  description = "Kaitai Struct C++ STL Runtime Library";
-                  homepage = "https://kaitai.io/";
-                  license = licenses.mit;
-                  maintainers = with maintainers; [ fmzakari ];
-                };
+              src = prev.fetchFromGitHub {
+                owner = "kaitai-io";
+                repo = "kaitai_struct_cpp_stl_runtime";
+                rev = "${finalAttrs.version}";
+                sha256 = "sha256-2glGPf08bkzvnkLpQIaG2qiy/yO+bZ14hjIaCKou2vU=";
               };
-            })
-          ];
-        };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = [
-            pkgs.kaitai-struct-compiler
-            pkgs.kaitai-struct-cpp-stl-runtime
-          ];
-        };
-      });
+
+              nativeBuildInputs = [
+                prev.cmake
+                prev.gtest
+                prev.zlib.dev
+                prev.copyPkgconfigItems
+              ];
+
+              pkgconfigItems = [
+                (prev.makePkgconfigItem rec {
+                  name = "kaitai-struct-cpp-stl-runtime";
+                  inherit (finalAttrs) version;
+                  cflags = ["-I${variables.includedir}"];
+                  libs = [
+                    "-L${variables.libdir}"
+                    "-lkaitai_struct_cpp_stl_runtime"
+                  ];
+                  variables = rec {
+                    prefix = placeholder "out";
+                    includedir = "${prefix}/include";
+                    libdir = "${prefix}/lib";
+                  };
+                  inherit (finalAttrs.meta) description;
+                })
+              ];
+
+              meta = with prev.lib; {
+                description = "Kaitai Struct C++ STL Runtime Library";
+                homepage = "https://kaitai.io/";
+                license = licenses.mit;
+                maintainers = with maintainers; [fmzakari];
+              };
+            });
+          })
+        ];
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = [
+          pkgs.kaitai-struct-compiler
+          pkgs.kaitai-struct-cpp-stl-runtime
+          pkgs.meson
+          pkgs.ninja
+          pkgs.pkg-config
+        ];
+      };
+    });
 }
